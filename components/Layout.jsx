3705eef4909121d900c0491c35e0d948e8ffd001
Diego Nery <79265846+DioNery@users.noreply.github.com>
2024-06-10 17:52:45 -0300

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -68,7 +68,7 @@ export default function Layout({ children }) {
                 <button
                   className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
                   onClick={() => {
-                    window.location.href = "https://new.safernet.org.br/"; // Redireciona para o site
+                    window.location.href = "https://new.safernet.org.br/denuncie"; // Redireciona para o site
                   }}
                 >
                   Continuar
